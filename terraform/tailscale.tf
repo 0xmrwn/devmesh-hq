3f0f997ba0b83e6316bac26eae5f9eca78669b3c
0xmrwn <niches-anode0k@icloud.com>
2025-08-06 19:02:11 +0200

    Remove unused Tailscale resources from Terraform configuration and state to streamline setup

--- terraform/tailscale.tf
+++ terraform/tailscale.tf
@@ -1,16 +1,3 @@
-resource "tailscale_acl" "as_json" {
-  acl = local.tailscale_acl
-}
-
-resource "tailscale_dns_preferences" "magic_dns" {
-  magic_dns = true
-}
-
-resource "tailscale_tailnet_settings" "tailnet_settings" {
-  acls_externally_managed_on = true
-  devices_auto_updates_on    = true
-}
-
 resource "tailscale_tailnet_key" "nodes_auth_key" {
   reusable            = true
   preauthorized       = true
